f2223cc263d95a0c43d13ce8d50076227f7b3fa1
Karla Rosas <k2.rosas.v@gmail.com>
2023-12-29 16:53:01 -0600

    Add basic rock paper scissors functionality and test case

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function getComputerChoice() {
     const MOVES = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * 3);
 
-
     return MOVES[index];
 }
 
@@ -11,5 +10,18 @@ function getPlayerChoice() {
     return playerChoice.toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+function play(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            console.log("You lose! Paper beats Rock");
+        } else if (computerSelection === "scissors") {
+            console.log("You win! Rock beats scissors");
+        } else {
+            console.log("It's a tie");
+            play(getPlayerChoice(), getComputerChoice());
+        }
+    }
+}
+
+
+play(getPlayerChoice(), getComputerChoice());
