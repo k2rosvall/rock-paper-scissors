bd72facffc2fc4b9ddb48a6cc403ec81a1844ab4
Karla Rosas <k2.rosas.v@gmail.com>
2023-12-29 17:40:36 -0600

    Modify game function to stop the game if someone won three games

--- script.js
+++ script.js
@@ -26,32 +26,31 @@ function evaluateChoices(playerSelection, computerSelection) {
     }
     else if ( WINNING_COMBINATIONS.includes(combination)) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return 0;
+        return "player";
     } else {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return 1;
+        return "computer";
     }
 }
 
 function game() {
-    let playerWins = 0;
-    let computerWins = 0;
+    let playerWins, computerWins = 0;
     for(let i = 0; i < 5; i++) {
-        let won = evaluateChoices(getPlayerChoice(), getComputerChoice());
-        if (won === 0) {
+        let winner = evaluateChoices(getPlayerChoice(), getComputerChoice());
+        if (winner === "player") {
             playerWins++;
             console.log(`Round ${i + 1}: Player wins`);
         } else {
             computerWins++
             console.log(`Round ${i + 1}: Computer wins`);
         }
-    }
 
-    if (playerWins > computerWins) {
-        console.log("Player wins");
-    } else if (computerWins > playerWins){
-        console.log("Computer wins")
-    } else {
-        console.log("it's a tie!")
+        if (playerWins === 3) {
+            console.log("Player wins the game!");
+            i = 5;
+        } else if (computerWins === 3) {
+            console.log("Computer wins the game!");
+            i = 5;
+        }
     }
 }
