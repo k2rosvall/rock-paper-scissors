826d67f9f1cb3c17b2e2abf2beb7c24b7e075650
Karla Rosas <k2.rosas.v@gmail.com>
2023-12-29 16:51:59 -0600

    Add function to prompt user for a choice

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ function getComputerChoice() {
     const MOVES = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * 3);
 
-    console.log(MOVES[index]);
 
     return MOVES[index];
 }
 
+function getPlayerChoice() {
+    let playerChoice = prompt("Select rock, paper or scissors");
+    return playerChoice.toLowerCase();
+}
+
 console.log(getComputerChoice());
+console.log(getPlayerChoice());
