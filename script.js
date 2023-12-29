d31037bf2617467cee5bf118afcfc394117b43f5
Karla Rosas <k2.rosas.v@gmail.com>
2023-12-29 16:51:07 -0600

    Return choide

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-console.log("Hello World!")
-
 function getComputerChoice() {
     const MOVES = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * 3);
 
     console.log(MOVES[index]);
+
+    return MOVES[index];
 }
 
 console.log(getComputerChoice());
